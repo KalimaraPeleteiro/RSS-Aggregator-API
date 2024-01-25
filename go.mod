d3cbc9ab3091f5a015d50c536eddb576058df243
Kalimara_Peleteiro <kalimarapeleteiro@gmail.com>
2024-01-25 13:46:35 -0300

    Estabelecendo variáveis de ambiente

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/KalimaraPeleteiro/RSS-Aggregator
 
 go 1.21.6
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Olá, Mundo!")
+
+	// Carregando o Arquivo .env
+	godotenv.Load()
+
+	// Buscando a Porta que a aplicação irá executar.
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("Não consegui acessar a porta em .env!")
+	}
+
+	fmt.Println("Iniciando na Porta", port)
 }
